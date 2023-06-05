d868f20323983762e195914ddf10c3908b7c92ca
Joe85gr <info@rainingdreams.co.uk>
2023-06-05 18:58:41 +0100

    removed arm64 platform force from install script

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ docker rm --force $app_name || true
 
 # Build Docker image
 
-docker build -t $app_name:latest -f ./Dockerfile --platform arm64 .
+docker build -t $app_name:latest -f ./Dockerfile .
 
 # Run Docker container
 docker run -d --name $app_name -p ${PORT}:80 $app_name:latest
